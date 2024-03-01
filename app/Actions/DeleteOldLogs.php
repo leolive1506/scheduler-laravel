016f0889793e0d6e549ba791808ae1488c89e7c0
Leonardo Lopes <leonardolivelopes2@gmail.com>
2024-03-01 19:33:36 -0300

    feat: create schedule class with __invoke

---
+++ app/Actions/DeleteOldLogs.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Actions;
+
+class DeleteOldLogs
+{
+    public function __invoke()
+    {
+        info('Deletei os logs antigos');
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Actions\DeleteOldLogs;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -12,6 +13,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+        $schedule->call(new DeleteOldLogs())->everyMinute();
         // $schedule->command('inspire')->hourly();
     }
 
